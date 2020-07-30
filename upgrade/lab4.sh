86f8ac4f42bd4bc7f7f49dc7a3c9a84e7af95ee6
Roman Tkachenko <roman@gravitational.com>
2020-07-30 10:59:31 -0700

    Add lab 4

---
+++ upgrade/lab4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Please pass a path to the unpacked upgrade tarball, for example: ./lab2.sh /home/ubuntu/gravity-5.5.49"
+    exit 1
+fi
+
+echo "ACTIVATING SCENARIO #4...
+"
+
+path="$1"
+sudo $path/upload
+sudo $path/gravity upgrade --manual
+sudo $path/gravity agent shutdown >/dev/null 2>&1
+
+echo "
+SCENARIO #4 ACTIVATED.
+
+OBJECTIVE
+---------
+
+The upgrade has been launched in the manual mode.
+
+Your goal is to inspect the upgrage plan and step through a few phases.
+"
