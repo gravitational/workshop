5ce0b1e09a44d5fe6d027f0b32c9209db594647a
ZLewis <68337117+ZedLewis@users.noreply.github.com>
2020-08-04 16:55:01 -0400

    Update lab7-fix.sh
    Fixed if/then statement.

--- upgrade/lab7-fix.sh
+++ upgrade/lab7-fix.sh
@@ -6,8 +6,10 @@ if which chronyd > /dev/null 2>&1
 then
     sudo systemctl enable --now chronyd >/dev/null 2>&1
 elif which ntpd > /dev/null 2>&1
+then
     sudo systemctl enable --now ntp >/dev/null 2>&1
 else
     echo "Please re-enable the system time correction daemon (NTP or similar)"
+fi
 
 echo "SCENARIO #6 ROLLED BACK"
